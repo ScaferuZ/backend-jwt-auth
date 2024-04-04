b79560e4e35330d43223d51a28a5f194ddc71150
ScaferuZ <endrafruz@gmail.com>
2024-04-04 10:46:58 +0700

    feat: adding userSignup and userLogin controller for authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,34 @@ const generateToken = (_id) => {
   );
 };
 
-const user_login = async (req, res) => {
+const userSignup = async (req, res) => {
+  console.log("Signing up");
+  const { email, password } = req.body; // requesting email and password from the body
+  try {
+    // "User" is a schema that we created and we  perform the database operations
+    const user = await User.signup(email, password);
+    console.log("generating token");
+    const token = generateToken(user._id);
+    console.log("Token generated =" + token);
+    res.status(201).json({ email, token }); // send back email and token as a response
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
+const userLogin = async (req, res) => {
   console.log("Signing in...");
-  const body = req.body;
-  res.status(200).json(body);
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = generateToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  userLogin,
+  userSignup,
 };
